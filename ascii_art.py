976353bbe12bf14c3d477f338d4de926322b62ab
ChaosCoder25 <mwill908@evit.edu>
2024-08-08 13:39:35 -0700

    I did 3 art works

---
+++ ascii_art.py
@@ -0,0 +1,12 @@
+print("               .  ___")
+print("           .     /  |")
+print("      .         /  =|")
+print("        .      /   =`.")
+print("     .        /      |")
+print("    .     _,.----'__`+  " )
+print("   .    -'------:_____]      .LS     Sienar Fleet Systems")
+print("               _|_                Lambda-class Imperial Shuttle ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("star wars rules! ")
+       
+       
